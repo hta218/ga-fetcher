a6acc8f2cd70bf2fd80af0324d4680231c1c717e
Tuan Anh Huynh <huynhtuananh21895@gmail.com>
2020-02-19 10:42:40 +0700

    Format auth

--- auth.js
+++ auth.js
@@ -15,7 +15,7 @@ const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_U
 
 const redirectURL = oauth2Client.generateAuthUrl({
   access_type: 'offline',
-	scope: 'https://www.googleapis.com/auth/analytics.readonly'
+  scope: 'https://www.googleapis.com/auth/analytics.readonly'
 })
 
 router.get('/signin', (req, res) => res.redirect(redirectURL))
